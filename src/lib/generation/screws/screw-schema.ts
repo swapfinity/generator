28165e421524330ba20e64548eb2c5646f848fde
RobinGuth <robin.guth@gmx.de>
2026-03-07 12:49:06 +0100

    tmp

--- src/lib/generation/screws/screw-schema.ts
+++ src/lib/generation/screws/screw-schema.ts
@@ -2,7 +2,7 @@ import { z, ZodObject } from "zod";
 
 // schemas
 const screwDrive = z.enum(["PH", "SLOT", "CROSS", "SQUARE", "HEX_SOCKET", "TORX"]).default("TORX").describe("The drive type of the screw.").meta({ viewName: "Screw Drive", rowWeight: 1, rowName: "screwDrive" })
-const screwType = z.enum(["FLAT_HEAD", "ROUND_HEAD", "PAN_HEAD"]).default("ROUND_HEAD").describe("The type of the screw.").meta({ viewName: "Screw Type", rowWeight: 1, rowName: "screwType" })
+const screwType = z.enum(["FLAT_HEAD", "ROUND_HEAD", "PAN_HEAD"]).default("PAN_HEAD").describe("The type of the screw.").meta({ viewName: "Screw Type", rowWeight: 1, rowName: "screwType" })
 
 const screwDriveText = z.string().default("").meta({ viewName: "Screw Drive Description Text (Optional)", rowWeight: 5, rowName: "screwDrive" }).describe("The text specifying the drive type of the screw.")
 const screwHeadTypeText = z.string().default("").meta({ viewName: "Screw Description Text (Optional)", rowWeight: 5, rowName: "screwType" }).describe("The main text describing the screw.")
--- src/lib/generation/screws/screw-type-icon-gen.ts
+++ src/lib/generation/screws/screw-type-icon-gen.ts
@@ -1,31 +1,39 @@
 import type { Geom2 } from "@jscad/modeling/src/geometries/types"
 import * as jscad from '@jscad/modeling';
-import type { screwType } from "./screw-schema";
+import type { ScrewType } from "./screw-schema";
+import { flatArc } from "../geom-utils";
 const { booleans, transforms, primitives, utils, expansions, hulls } = jscad;
 
 
-export const getScrewTypeIcon = (screwType: screwType): Geom2 => {
+export const getScrewTypeIcon = (screwType: ScrewType): Geom2 => {
     return getIconForScrewType(screwType)
 }
 
-const getIconForScrewType = (screwType: screwType): Geom2 => {
+const getIconForScrewType = (screwType: ScrewType): Geom2 => {
     switch (screwType) {
         case "FLAT_HEAD":
             return generateFlatHeadIcon()
         case "ROUND_HEAD":
             return generateRoundHeadIcon()
         case "PAN_HEAD":
+            return generatePanHeadIcon()
         default:
             throw new Error("Invalid screw type")
     }
 }
 
 // helpers - consts
-export const SCREW_LENGTH = 20
-export const SCREW_WIDTH = 2.7
+export const SCREW_SHANK_LENGTH = 20
+export const SCREW_SHANK_WIDTH = 2.7
 
 const FLAT_HEAD_HEAD_WIDTH = 6.5
 const ROUND_HEAD_HEAD_RADIUS = 3
+
+const PAN_HEAD_HEAD_WIDTH = 5.4
+const PAN_HEAD_HEAD_HEIGHT = 1
+const PAN_HEAD_HEAD_ROUNDING = 0.2
+const PAN_HEAD_HEAD_ARC_HEIGHT = 1
+
 
 const segments = 64
 
@@ -33,7 +41,7 @@ const SCREW_ROUNDING = 0.3
 
 // helpers - functions
 const generateDefaultScrewShank = (): Geom2 => {
-    return primitives.roundedRectangle({ size: [SCREW_LENGTH, SCREW_WIDTH], roundRadius: SCREW_ROUNDING })
+    return primitives.roundedRectangle({ size: [SCREW_SHANK_LENGTH, SCREW_SHANK_WIDTH], roundRadius: SCREW_ROUNDING })
 }
 
 const generateFlatHeadIcon = (): Geom2 => {
@@ -50,7 +58,7 @@ const generateFlatHeadIcon = (): Geom2 => {
         )
     )
     const rotatedScrewHead = transforms.rotateZ(utils.degToRad(-90), screwHead)
-    const repositionedScrewHead = transforms.translate([-SCREW_LENGTH / 2, FLAT_HEAD_HEAD_WIDTH / 2], rotatedScrewHead)
+    const repositionedScrewHead = transforms.translate([-SCREW_SHANK_LENGTH / 2, FLAT_HEAD_HEAD_WIDTH / 2], rotatedScrewHead)
     return booleans.union(screwShank, repositionedScrewHead)
 }
 
@@ -66,10 +74,33 @@ const generateRoundHeadIcon = (): Geom2 => {
             )
         )
     )
-    const repositionedScrewHead = transforms.translate([-(SCREW_LENGTH / 2 - ROUND_HEAD_HEAD_RADIUS + 0.3),], screwHead)
+    const repositionedScrewHead = transforms.translateX(-(SCREW_SHANK_LENGTH / 2 - ROUND_HEAD_HEAD_RADIUS + 0.3), screwHead)
 
 
     return booleans.union(repositionedScrewHead, screwShank)
 }
 
+const generatePanHeadIcon = (): Geom2 => {
+    const screwShank = generateDefaultScrewShank()
 
+    const fullHeadHeightOffset = PAN_HEAD_HEAD_HEIGHT / 2 + PAN_HEAD_HEAD_ARC_HEIGHT / 2
+    const screwHeadTop = flatArc(PAN_HEAD_HEAD_WIDTH, PAN_HEAD_HEAD_ARC_HEIGHT, segments)
+    const rotatedScrewAndRepositionedHeadTop = transforms.translateX(
+        -(SCREW_SHANK_LENGTH / 2), transforms.rotateZ(utils.degToRad(90), screwHeadTop)
+    )
+    const screwHeadMain = primitives.rectangle({ size: [PAN_HEAD_HEAD_HEIGHT, PAN_HEAD_HEAD_WIDTH] })
+    const repositionedScrewHeadMain = transforms.translateX(-(SCREW_SHANK_LENGTH / 2 - fullHeadHeightOffset), screwHeadMain)
+
+    const fullHead = expansions.offset(
+        { delta: PAN_HEAD_HEAD_ROUNDING, corners: "round", segments: segments },
+        expansions.offset(
+            { delta: PAN_HEAD_HEAD_ROUNDING, corners: "round", segments: segments },
+            booleans.union(
+                rotatedScrewAndRepositionedHeadTop,
+                repositionedScrewHeadMain
+            )
+        )
+    )
+
+    return booleans.union(fullHead, screwShank)
+}
